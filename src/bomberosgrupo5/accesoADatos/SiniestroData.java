c093715e5db17a5865dd28e3cb53e3c8e3b43354
fey1975 <fabian.yapura@gmail.com>
2023-11-08 22:32:33 -0300

    3.siniestro

--- src/bomberosgrupo5/accesoADatos/SiniestroData.java
+++ src/bomberosgrupo5/accesoADatos/SiniestroData.java
@@ -49,41 +49,4 @@ public class SiniestroData {
             JOptionPane.showMessageDialog(null, "Error al acceder a la Tabla siniestro: " + ex.getMessage());
         }
     }
-
-//------------------------------------------------------------------------------    
-//public void guardarSiniestro(Siniestro siniestro) {
-//
-//        String sql = "INSERT INTO siniestro (tipo,fechaSiniestro,horaSiniestro,coordx,coordy,detalles,fechaResol,puntuacion,idBrigada)"
-//                + "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-//
-//        try {
-//            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-//
-//        ps.setString(1, siniestro.getTipo());
-//        ps.setDate(2, Date.valueOf(siniestro.getFechaSiniestro()));
-//        ps.setTime(3, Time(siniestro.getHoraSiniestro()));
-//        ps.setString(4, siniestro.getCoordX());
-//        ps.setString(5, siniestro.getCoordY());
-//        ps.setString(6, siniestro.getDetalles());
-//        ps.setDate(7, Date.valueOf(siniestro.getFechaResol()));
-//        ps.setInt(8, siniestro.getPuntuacion());
-//        ps.setInt(9, siniestro.getBrigada());
-//            
-//            
-//            ps.executeUpdate();
-//
-//            ResultSet rs = ps.getGeneratedKeys();
-//
-//            if (rs.next()) {
-//                siniestro.setIdSiniestro(rs.getInt(1));
-//                JOptionPane.showMessageDialog(null, "Siniestro Agregado Exitosamente ");
-//            }
-//            ps.close();
-//
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(null, "Error al acceder a la Tabla siniestro ");
-//        }
-//
-//    }
-////------------------------------------------------------------------------------
 }
